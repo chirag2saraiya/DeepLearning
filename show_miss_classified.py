80432456075556f630ba17806ff56d0626458d77
chirag2saraiya <chirag2saraiya@gmail.com>
2019-10-13 21:08:38 +0530

    Create show_miss_classified.py

---
+++ show_miss_classified.py
@@ -0,0 +1,7 @@
+#block_row : rows in image block grid
+#block_column: column in image block grid
+#type : misclassified/ParticularClass/Random/MisclassifiedClass
+
+def showAndPlotImage(block_row, block_column, type, class):
+
+  
