b9111a638bd520cd16e71e010f2ef772d70787d1
chirag2saraiya <chirag2saraiya@gmail.com>
2019-10-13 22:02:12 +0530

    Update and rename show_miss_classified.py to show_mis_classified.py

---
+++ show_mis_classified.py
@@ -0,0 +1,26 @@
+#block_row : rows in image block grid
+#block_column: column in image block grid
+#type : misclassified/ParticularClass/Random/MisclassifiedClass
+
+def showAndPlotImage(rows, columns, type, class, y_pred, y_test,x_test):
+  if(type == "misclassified"):
+    # Generate a rows x cols sized image grid 
+    f, axarr = plt.subplots(rows,cols)
+
+    i = 0
+    count = 0
+    while ((count < rows*cols) and (i < x_test.shape[0])):
+       if(predictedDigits[i] != y_test[i]):
+            axarr[count // rows][count % cols].imshow(x_test[i])
+            axarr[count // rows][count % cols].set_title('Predicted:{} / Actual:{}'.format(predictedDigits[i],y_test[i]),fontsize=12)
+            axarr[count // rows][count % cols].axis('off')
+            count += 1
+       
+       i +=  1
+                 
+    f.subplots_adjust(hspace=0.2)    
+    f.suptitle('List of Missclassified images', fontsize=25)
+    f.set_size_inches(15,15)
+    
+
+  
